49c88fffa63f09a014ef6794ec98e6d89c9c2485
Diablohu <huxin@cmcm.com>
2024-04-03 15:11:29 +0800

    changed all bins' line breaks to LF

--- packages/eslint-config-koot/index.js
+++ packages/eslint-config-koot/index.js
@@ -170,8 +170,8 @@ module.exports = {
             rules: Object.assign(
                 {},
                 // tsRecommended1.overrides[0].rules,
-                // tsRecommended1().rules,
-                tsRecommended2.rules,
+                tsRecommended1?.()?.rules || {},
+                tsRecommended2?.rules || {},
                 {
                     '@typescript-eslint/no-angle-bracket-type-assertion': 0,
                 }
